4454a459fe478b7aee9f3db6dc45c7b635e2dee7
n-kazachuk <nikita955555@mail.ru>
2024-03-01 16:13:45 +0300

    Add commander

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bot/internal/app/commands"
 	"bot/internal/service/product"
 	"github.com/joho/godotenv"
 	"log"
@@ -28,46 +29,22 @@ func main() {
 	}
 
 	updates := bot.GetUpdatesChan(u)
+
 	productService := product.NewService()
+	commander := commands.NewCommander(bot, productService)
 
 	for update := range updates {
-		if update.Message != nil {
+		if update.Message == nil {
 			continue
 		}
 
 		switch update.Message.Command() {
 		case "help":
-			helpCommand(bot, update.Message)
+			commander.Help(update.Message)
 		case "list":
-			listCommand(bot, update.Message, productService)
+			commander.List(update.Message)
 		default:
-			defaultBehavior(bot, update.Message)
+			commander.Default(update.Message)
 		}
 	}
-}
-
-func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "/help - help \n /list - list products")
-	bot.Send(msg)
-}
-
-func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message, productService product.Service) {
-	outputMsgText := ""
-
-	products := productService.List()
-	for _, p := range products {
-		outputMsgText += p.Title
-		outputMsgText += "\n"
-	}
-
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMsgText)
-
-	bot.Send(msg)
-}
-
-func defaultBehavior(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
-
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, inputMessage.Text)
-	bot.Send(msg)
 }
